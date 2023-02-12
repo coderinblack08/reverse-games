41d577da5039da6f4be42ba2251ae3dd771f0438
coderinblack08 <kevinlu.email@gmail.com>
2023-02-11 19:24:00 -0800

    flipping connect 4

--- pages/connect4.tsx
+++ pages/connect4.tsx
@@ -1,48 +1,28 @@
-import { Box, Flex, Heading, HStack, useToast, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
+import { create } from "zustand";
+import { shallow } from "zustand/shallow";
 
-const Connect4Column = ({ col, simulateTurn, board }: any) => {
-  return (
-    <VStack
-      display="inline-flex"
-      p={3}
-      backgroundColor="#5E6398"
-      spacing={6}
-      onClick={() => simulateTurn(col)}
-      cursor="pointer"
-    >
-      {new Array(8).fill(null).map((cell: any, row: number) => (
-        <Box
-          boxSize={12}
-          backgroundColor={
-            board[row][col] === 1
-              ? "red"
-              : board[row][col] === 0
-              ? "yellow"
-              : "white"
-          }
-          borderRadius="full"
-          key={row}
-        />
-      ))}
-    </VStack>
-  );
-};
-
-const Connect4Page: NextPage = () => {
-  const [turn, setTurn] = useState(1); // 1 = red, 0 = blue
-  const [board, setBoard] = useState<any>(
-    new Array(8).fill(new Array(4).fill(null))
-  );
-
-  function updateCell(row: number, col: number, player: number) {
-    const newBoard = board.map((row: any) => [...row]);
-    newBoard[row][col] = player;
-    setBoard(newBoard);
-  }
-
-  const checkState = useCallback(() => {
+const useBoardStore = create((set, get) => ({
+  board: new Array(8).fill(new Array(4).fill(null)),
+  turn: 1,
+  simulateTurn: (row: number, col: number) => {
+    set((state: any) => {
+      const newBoard = state.board.map((row: any) => [...row]);
+      newBoard[row][col] = state.turn;
+      return { board: newBoard, turn: (state.turn + 1) % 2 };
+    });
+  },
+  checkState: () => {
+    const { board } = get() as any;
     // check horizontal
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 4; col++) {
@@ -98,41 +78,92 @@ const Connect4Page: NextPage = () => {
         }
       }
     }
-
     return null;
-  }, [board]);
-
-  const checkIfDraw = useCallback(() => {
+  },
+  checkIfDraw: () => {
+    const { board, checkState } = get() as any;
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 4; col++) {
-        if (board[row][col] === null) {
-          return false;
-        }
+        if (board[row][col] === null) return false;
       }
     }
     return checkState() === null;
-  }, [board, checkState]);
-
-  function simulateTurn(col: number) {
-    for (let row = 7; row >= 0; row--) {
-      if (board[row][col] === null) {
-        updateCell(row, col, turn);
-        break;
+  },
+  updateCell: (row: number, col: number, player: number) => {
+    set((state: any) => {
+      const newBoard = state.board.map((row: any) => [...row]);
+      newBoard[row][col] = player;
+      return { board: newBoard };
+    });
+  },
+  flipColumn: (col: number) =>
+    set((state: any) => {
+      const newBoard = state.board.map((row: any) => [...row]);
+      // swap rows 0 and 7, 1 and 6, 2 and 5, 3 and 4
+      for (let i = 0; i < 4; i++) {
+        const temp = newBoard[i][col];
+        newBoard[i][col] = newBoard[7 - i][col];
+        newBoard[7 - i][col] = temp;
       }
-    }
-    flipColumn(col);
-    setTurn(turn === 1 ? 0 : 1);
+      return { board: newBoard };
+    }),
+}));
+
+const Connect4Column = ({ col }: any) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [board, simulateTurn, flipColumn] = useBoardStore(
+    (state: any) => [state.board, state.simulateTurn, state.flipColumn],
+    shallow
+  );
+
+  function animate() {
+    ref.current?.animate(
+      [{ transform: "rotateX(0deg)" }, { transform: "rotateX(180deg)" }],
+      { duration: 500, iterations: 1, easing: "ease-in-out" }
+    );
   }
 
-  function flipColumn(col: number) {
-    for (let row = 0; row < 4; row++) {
-      const temp = board[row][col];
-      board[row][col] = board[7 - row][col];
-      board[7 - row][col] = temp;
-    }
-  }
+  return (
+    <VStack
+      display="inline-flex"
+      p={3}
+      backgroundColor="#5E6398"
+      spacing={6}
+      ref={ref}
+    >
+      {new Array(8).fill(null).map((cell: any, row: number) => (
+        <Button
+          variant="unstyled"
+          disabled={board[row][col] !== null}
+          boxSize={12}
+          onClick={() => {
+            if (board[row][col] !== null) return;
+            simulateTurn(row, col);
+            animate();
+            setTimeout(() => flipColumn(col), 500);
+          }}
+          cursor="pointer"
+          backgroundColor={
+            board[row][col] === 1
+              ? "red"
+              : board[row][col] === 0
+              ? "yellow"
+              : "white"
+          }
+          borderRadius="full"
+          key={row}
+        />
+      ))}
+    </VStack>
+  );
+};
 
+const Connect4Page: NextPage = () => {
   const toast = useToast();
+  const [board, checkState, checkIfDraw] = useBoardStore(
+    (state: any) => [state.board, state.checkState, state.checkIfDraw],
+    shallow
+  );
 
   useEffect(() => {
     if (checkIfDraw()) {
@@ -146,18 +177,11 @@ const Connect4Page: NextPage = () => {
   return (
     <Box minH="100vh" p={4}>
       <Heading textAlign="center" size="lg">
-        Connect4 Flip
+        Connect 4 Flip
       </Heading>
       <HStack justifyContent="center" mt={4} spacing={4}>
         {new Array(4).fill(null).map((_, i: number) => (
-          <Connect4Column
-            key={i}
-            col={i}
-            board={board}
-            simulateTurn={simulateTurn}
-            updateCell={updateCell}
-            flipColumn={flipColumn}
-          />
+          <Connect4Column key={i} col={i} />
         ))}
       </HStack>
     </Box>
